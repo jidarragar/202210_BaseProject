fa977ccb41034f62eb5d0f2ff2a9199f070939f8
jidarragar <j.idarragar@uniandes.edu.co>
2022-05-01 17:17:08 -0500

    Desarrollo punto No 2 - Actualizacion funcionalidad Vehiculo

--- src/app/car/car-list/car-list.component.ts
+++ src/app/car/car-list/car-list.component.ts
@@ -5,21 +5,39 @@ import { CarService } from '../car.service';
 @Component({
   selector: 'app-car-list',
   templateUrl: './car-list.component.html',
-  styleUrls: ['./car-list.component.css']
+  styleUrls: ['./car-list.component.css'],
 })
 export class CarListComponent implements OnInit {
+  cars: Array<Car> = [];
+  summaryCars: Array<{ marca: string; total: number }> = [];
+  constructor(private carService: CarService) {}
 
-  cars: Array<Car>=[];
-  constructor(private carService:CarService) { }
+  getCars(): void {
+    this.carService.getCars().subscribe((cars) => {
+      this.cars = cars;
 
-  getCars(): void{
-    this.carService.getCars().subscribe((cars) =>{
-      this.cars=cars;
+      this.getSummary();
     });
+  }
+
+  getSummary(): void {
+    this.summaryCars = Object.values(
+      this.cars.reduce(
+        (summary: Array<{ marca: string; total: number }>, currentCar) => {
+          if (!summary[currentCar.marca as any])
+            summary[currentCar.marca as any] = {
+              marca: currentCar.marca,
+              total: 1,
+            };
+          else summary[currentCar.marca as any].total += 1;
+          return summary;
+        },
+        []
+      )
+    );
   }
 
   ngOnInit() {
     this.getCars();
   }
-
 }
